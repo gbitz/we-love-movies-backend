b5b11cb23de0e81b4a1f6f9094a6069dee14a014
Grant Bitzer <gbitzer86@gmail.com>
2022-05-15 15:06:54 -0500

    added movies router to app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const moviesRouter = require("./movies/movies.router");
+
 const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+
 
 module.exports = app;
