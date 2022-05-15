fcad854a5aeb1e1b2b6dcef7f1168c656993aa95
Grant Bitzer <gbitzer86@gmail.com>
2022-05-15 13:02:08 -0500

    added CREATE movies_theaters table migration

---
+++ src/db/migrations/20220515125636_createMoviesTheatersTable.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+    return knex.schema.creatTable("movies_theaters", (table) => {
+        table
+            .foreign("movie_id")
+            .references("movie_id")
+            .inTable("movies")
+            .onDelete("cascade");
+        table
+            .foreign("theater_id")
+            .references("theater_id")
+            .inTable("theaters")
+            .onDelete("cascade");
+        table.boolean("is_showing")
+    })
+};
+
+exports.down = function(knex) {
+    return knex.shcema.dropTable("movies_theaters");
+};
