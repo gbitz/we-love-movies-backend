76b00c9d34ea967b58e87141b853621a9c29d7a2
Grant Bitzer <gbitzer86@gmail.com>
2022-05-17 15:00:07 -0500

    added delete control to review route

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,6 +1,25 @@
 const reviewService = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-module.exports = {
+async function reviewExists(req,res,next) {
+    const {reviewId} =  req.params;
+    const review = await reviewService.read(reviewId);
+    if (review) {
+        res.locals.review = review;
+        return next();
+    }
+    return next({status:404, message: `Review cannot be found.`})
+}
 
+async function deleteReview(req,res,next) {
+    const {review} = res.locals;
+    await reviewService.delete(review.review_id)
+    res.sendStatus({status:204, message:"No Content"})
+}
+
+module.exports = {
+    deleteReview : [
+        reviewExists,
+        asyncErrorBoundary(deleteReview)
+    ]
 }
--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -3,6 +3,8 @@ const controller = require("/reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
+    .route("/:reviewId")
+    .delete(controller.delete)
     .all(methodNotAllowed)
 
 module.exports = router;
