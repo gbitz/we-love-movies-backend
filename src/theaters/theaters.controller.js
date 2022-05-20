5fdf073bf51e0f3c1aa353c20bfccc785427a0c1
Grant Bitzer <gbitzer86@gmail.com>
2022-05-20 11:19:27 -0500

    added boilerplate router/controller/service for theaters route

---
+++ src/theaters/theaters.controller.js
@@ -0,0 +1,8 @@
+const theaterService = reqire("./theater.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
+
+module.exports = {
+
+}
---
+++ src/theaters/theaters.router.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const controller= require("./theaters.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed")
+
+
+
+module.exports = router;
