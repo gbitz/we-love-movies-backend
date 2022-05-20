d51a392ff4efe997affee943cb81b35733b202c5
Grant Bitzer <gbitzer86@gmail.com>
2022-05-20 12:20:04 -0500

    add list theaters with associated movies to theaters route

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,8 +1,27 @@
 const knex = require("../db/connection");
+const reduceProperties =  require("../utils/reduce-properties");
+
+const addMovies = reduceProperties("theater_id", {
+    movie_id: ["movies", null, "movie_id"],
+    title: ["movies", null, "title"],
+    runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+    rating: ["movies", null, "rating"],
+    description: ["movies", null, "description"],
+    image_url: ["movies", null, "image_url"],
+    created_at: ["movies", null, "created_at"],
+    updated_at: ["movies", null, "updated_at"],
+    is_showing: ["movies", null, "is_showing"],
+}) 
 
 function list() {
-    return knex("theaters").select("*")
+    return knex("theaters as t")
+        .join("movies_theaters as mt", "mt.theater_id", "t.theater_id")
+        .join("movies as m", "m.movie_id", "mt.movie_id")
+        .select("t.*", "m.*")
+        .then(theaters => addMovies(theaters))
 }
+
+
 
 module.exports = {
     list,
