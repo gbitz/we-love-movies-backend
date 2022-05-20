80fcb9e911491c5bff67cbd6820238b4e634d27b
Grant Bitzer <gbitzer86@gmail.com>
2022-05-20 12:27:25 -0500

    placed error handlers into seperate files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const errorHandler = require("./errors/errorHandler");
+const notFoundHandler = require("./errors/notFoundHandler");
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
@@ -15,15 +17,9 @@ app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter)
 
 // Not found handler
-app.use((req, res, next) => {
-    next({ status: 404, message: `Not found: ${req.originalUrl}` });
-});
+app.use(notFoundHandler);
   
 // Error handler
-app.use((error, req, res, next) => {
-console.error(error);
-const { status = 500, message = "Something went wrong!" } = error;
-res.status(status).json({ error: message });
-});
+app.use(errorHandler)
 
 module.exports = app;
